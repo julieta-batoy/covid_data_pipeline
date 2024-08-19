7f57fadf7b45a326703d989591baa8c9e8774bb3
Julieta Batoy <25427201+julieta-batoy@users.noreply.github.com>
2024-08-19 17:43:12 +0800

    Update setup_postgres.sql

--- scripts/setup_postgres.sql
+++ scripts/setup_postgres.sql
@@ -9,3 +9,6 @@ CREATE USER user WITH PASSWORD 'password';
 -- Grant all privileges to the user on the new database
 -- Replace 'database_name' and 'user' with the actual database name (e.g., staging_covid_db) and (e.g., user123)
 GRANT ALL PRIVILEGES ON DATABASE database_name TO user;
+
+-- Replace 'database_name' and 'user' with the actual database name (e.g., staging_covid_db) and (e.g., user123)
+ALTER DATABASE database_name OWNER TO user;
